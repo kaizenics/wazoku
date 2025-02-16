04ee32927b7c244036ecb8f3e875cebbd9af7270
kaizenics <nicosejohnsoriano@gmail.com>
2025-02-16 22:50:13 +0800

    feat: tailwindcss setup for php

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./*.php", "./**/*.php", "./**/*.{php,html,js}", "./template-parts/*.{php,html,js}", ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
